ffb8157be947cb3d89d6d319e0838b1a3b62a128
CCastro <chris502castro@gmail.com>
2018-11-08 12:34:22 -0700

    Significant functions added. The last function, unscrambledWord, is the major function needing improvement, rest is simpler.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,65 @@
-const wordArray = ["lowly", "start", "chilly", "bun", "bead", "friend", "return", "view", "cloth", "frogs", "celery", "basin", "stand", "special", "broad", "abaft", "plead", "quartz", "mark", "tempt", "shop", "stone", "scorch", "taboo", "hoax", "spiffy", "insure"];
+var userGuesses = [];
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var unknownWord = "";
 
-let guessedLetters = [""];
+function wordGenerator() {
+    var computerChoices = ["lowly", "start", "chilly", "bun", "bead", "friend", "return", "view", "cloth", "frogs", "celery", "basin", "stand", "special", "broad", "abaft", "plead", "quartz", "mark", "tempt", "shop", "stone", "scorch", "taboo", "hoax", "spiffy", "insure"];
+    var cpuWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(cpuWord);
+    return cpuWord;
+};
+var computerWord = wordGenerator();
 
-let guesses = 10;
-
-var randWord = "";
-
-function guessesAllowed(guessedLetters, key-pressed) {
-    // When a guess is made by the user, guesses is iterated down by 1
-
-    if (key - is - pressed) {
-        guesses = guesses--
-        guessedLetters.push(key - pressed)
+function scrambledWord(string) {
+    var knownWord = ""
+    if (string.length == 3) {
+        knownWord = "___"
+    } else if (string.length == 4) {
+        knownWord = "____"
+    } else if (string.length == 5) {
+        knownWord = "_____"
+    } else if (string.length == 6) {
+        knownWord = "______"
+    } else if (string.length == 7) {
+        knownWord = "_______"
     }
-    console.log(guesses)
-    gameRestart();
+    return knownWord;
 }
+var unknownWord = scrambledWord(computerWord);
 
-function findWord() {
-    var randWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-    console.log(randWord)
-}
+// Create variables that hold references to the places in the HTML where we want to display things.
+var directionsText = document.getElementById("directions-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var userGuessList = document.getElementById("userguesslist-text")
 
-function gameRestart() {
-    guesses = 10;
-    guessedLetters.length = 0;
-    findWord();
-}
+// This function is run whenever the user presses a key.
+document.onkeyup = function (event) {
 
-function wordCheck() {
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x")(userGuess === "y") || (userGuess === "z")) {
+    // Determines which key was pressed.
+    var userGuess = event.key;
+
+    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
         userGuesses.push(userGuess);
-        for (i = 0; i < computerGuess.length; i++) {
-            console.log(computerGuess[i]);
+        // // Hide the directions
+        directionsText.textContent = "";
+
+        // Display the user and computer guesses, and wins/losses/ties.
+        userChoiceText.textContent = "You chose: " + userGuess;
+        userGuessList.textContent = "You have guessed: " + userGuesses;
+        wordChecker(userGuess)
+    } else {
+        alert("You did not enter an alphabetical character.")
+    }
+};
+
+function wordChecker(input) {
+    console.log(input);
+    for (i = 0; i < computerWord.length; i++) {
+        if (computerWord.includes(input)) {
+            alert("You guessed a correct character")
         }
     }
 }
-
-
-function keyPress() {
-    starts when key is pressed.
-}
-
-function letterCheck(word) {
-    for (i = 0; i < word.length; i++) {
-
-    }
-}
-// array that holds the random words the game might play (wordArray). function (guessesAllowed) that holds a 'let' variable that contains the number of letter guesses allowed (guesses), starts at 10, calls on gameRestart function once guessesAllowed = 0. function (guessedLetters) that holds array that holds guessed letters (letters); when game restarts, function eliminates all values from the array. function to restart the game once 10 guesses has been made (gameRestart). function to find the random word(randWord) from wordArray. function to loop over letters in randWord with letter guessed by user. function that grabs the letter typed by user, and calls the function the loops over the word. looping function
