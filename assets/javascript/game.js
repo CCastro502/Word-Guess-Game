abbba2133298ede24a9345ec4eac627523fbb636
CCastro <chris502castro@gmail.com>
2018-11-13 12:29:12 -0700

    had forgotten to update code to what was in my portfolio. code is updated now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 var userGuesses = [];
+var guessesLeft = 10;
 var wins = 0;
 var losses = 0;
 var ties = 0;
 var unknownWord = "";
+var typeCounter = 0;
 
 function wordGenerator() {
     var computerChoices = ["lowly", "start", "chilly", "bun", "bead", "friend", "return", "view", "cloth", "frogs", "celery", "basin", "stand", "special", "broad", "abaft", "plead", "quartz", "mark", "tempt", "shop", "stone", "scorch", "taboo", "hoax", "spiffy", "insure"];
@@ -13,19 +15,12 @@ function wordGenerator() {
 var computerWord = wordGenerator();
 
 function scrambledWord(string) {
-    var knownWord = ""
-    if (string.length == 3) {
-        knownWord = "___"
-    } else if (string.length == 4) {
-        knownWord = "____"
-    } else if (string.length == 5) {
-        knownWord = "_____"
-    } else if (string.length == 6) {
-        knownWord = "______"
-    } else if (string.length == 7) {
-        knownWord = "_______"
-    }
-    return knownWord;
+
+    var knownWord = "_".repeat(string.length);
+    var arr = [...knownWord];
+    console.log(knownWord);
+    return arr;
+
 }
 var unknownWord = scrambledWord(computerWord);
 
@@ -33,45 +28,83 @@ var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
 var userGuessList = document.getElementById("userguesslist-text");
 var unscrambledWordText = document.getElementById("unscrambledword-text");
+var guessesLeftText = document.getElementById("guessesLeft-text")
 var winsText = document.getElementById("wins-text");
-var tiesText = document.getElementById("ties-text");
 var lossesText = document.getElementById("losses-text");
 
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
+    var userKeyCode = event.keyCode;
+    console.log("KeyCode = " + userKeyCode);
 
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
+    if (userKeyCode > 57 && userKeyCode < 91) {
         userGuesses.push(userGuess);
         directionsText.textContent = "";
 
+        wordChecker(userGuess);
         userChoiceText.textContent = "You chose: " + userGuess;
         userGuessList.textContent = "You have guessed: " + userGuesses;
         unscrambledWordText.textContent = "The word is: " + unknownWord;
+        guessesLeft.textContent = "# of guesses left: " + guessesLeft
         winsText.textContent = "Wins: " + wins;
-        tiesText.textContent = "Ties: " + ties;
         lossesText.textContent = "Losses: " + losses;
-        wordChecker(userGuess)
+        typeCounter++;
+        console.log(typeCounter);
+        guessesLeft--;
+        console.log(guessesLeft);
     } else {
         alert("You did not enter an alphabetical character.")
     }
 };
 
-function wordChecker(input) {
-    if (computerWord.includes(input)) {
-        alert("You guessed a correct character")
-        unscrambledWord(input, unknownWord, computerWord)
+function wordChecker(userGuess) {
+    if (computerWord.includes(userGuess)) {
+        alert("You guessed a correct character");
     } else {
         alert("You guessed an incorrect character")
     }
+    unscrambledWord(unknownWord, computerWord, userGuess);
 }
 
-function unscrambledWord(character, scrmbldword, word) {
-    for (k = 0; k < word.length; k++) {
-        if (character == word[k]) {
-            var tempLetter = word[k];
-            tempWord = scrmbldword.replace(scrmbldword[k], character);
-            console.log(tempWord);
+function unscrambledWord(unknownWord, word, userGuess) {
+    for (i = 0; i < word.length; i++) {
+        if (userGuess === word[i]) {
+            // unscrambledWordText.textContent = "The word is: " + unknownWord;
+            // winsText.textContent = "Wins: " + wins;
+            // lossesText.textContent = "Losses: " + losses;
+            unknownWord.splice(i, 1, word[i]);
+
         }
+    } checkGame(unknownWord, word);
+}
+
+unscrambledWordText.textContent = "The word is: " + unknownWord;
+
+function checkGame(unknownWord, word) {
+
+    if (guessesLeft === 0) {
+        alert("You lost!!! New game started");
+        computerWord = wordGenerator();
+        unknownWord.length = 0;
+        unknownWord = scrambledWord(computerWord);
+        userGuesses = [];
+        losses++;
+        guessesLeft = 10;
+    }
+    for (q = 0; q < word.length; q++) {
+        var userWin = true;
+        if (word[q] !== unknownWord[q]) {
+            var userWin = false;
+        }
+    } if (userWin === true) {
+        computerWord = wordGenerator();
+        unknownWord.length = 0;
+        unknownWord = scrambledWord(computerWord);
+        userGuesses = [];
+        wins++;
+        alert("You've won!!! New game started");
+        guessesLeft = 10;
+        return true;
     }
 }
